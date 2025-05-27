072de4e491f66c70fd39697d162e87e0b5366305
FrancescoMer <mercoglianofrancesco10@gmail.com>
2025-05-27 16:39:51 +0200

    commenti

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("hello");
+// Funzione che calcola il prezzo del biglietto
+
+// Ottieni i valori dagli input HTML
+ 
+// Prezzo per km
+
+// Controllo validità input
+
+ 
+// Avviso per input non valido
+    
+// Calcolo del prezzo base
+// Descrizione dello sconto applicato
+
+ // Applica sconto in base all'età
+
+
+// Prezzo con sconto applicato
+
+// Mostra il risultato
+
+}
